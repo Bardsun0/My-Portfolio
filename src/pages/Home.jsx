f9a8545b6ce01852c69cea4a32db18a747822b22
Ozan Güneş <ozangunes2894@gmail.com>
2024-08-08 02:49:02 +0300

    Navbar, header, skills done!!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { lazy, Suspense } from "react";
 
 const Navbar = lazy(() => import("../components/Navbar"));
 const Header = lazy(() => import("../components/Header"));
-const Skills = lazy(() => import('../components/Skills'));
+const Skills = lazy(() => import("../components/Skills"));
 
 const Home = () => {
   return (
